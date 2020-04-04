38fd851827e470abaa0808a4b77c3e4752a33e2a
Андрей Григорьев <greegav@gmail.com>
2020-04-04 13:22:12 +0300

    branch1

--- src/main/java/thymeleaf/byk/Car.java
+++ src/main/java/thymeleaf/byk/Car.java
@@ -30,30 +30,4 @@ public class Car {
     public void setMark(String mark) {
         this.mark = mark;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Car car = (Car) o;
-
-        if (model != null ? !model.equals(car.model) : car.model != null) return false;
-        return mark != null ? mark.equals(car.mark) : car.mark == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = model != null ? model.hashCode() : 0;
-        result = 31 * result + (mark != null ? mark.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "model='" + model + '\'' +
-                ", mark='" + mark + '\'' +
-                '}';
-    }
 }
